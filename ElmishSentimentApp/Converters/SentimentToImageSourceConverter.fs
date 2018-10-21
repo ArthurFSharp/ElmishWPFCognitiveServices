7491871f8512fcc07362b1879848dea2b13ce5a5
Arthur LE MEUR <arthur-lemeur.mslab@outlook.com>
2018-10-21 19:44:53 +0200

    add neutral state

--- ElmishSentimentApp/Converters/SentimentToImageSourceConverter.fs
+++ ElmishSentimentApp/Converters/SentimentToImageSourceConverter.fs
@@ -12,7 +12,8 @@ type SentimentToImageSourceConverter() =
                 | 0. -> box ""
                 | v when v > 0. && v < 25. -> box "Assets/rage.png"
                 | v when v >= 25. && v < 50. -> box "Assets/bad.png"
-                | v when v >= 50. && v < 70. -> box "Assets/ok.png"
+                | v when v >= 50. && v < 60. -> box "Assets/neutral.png"
+                | v when v >= 60. && v < 80. -> box "Assets/ok.png"
                 | _ -> box "Assets/good.png"
             with
             | _ -> DependencyProperty.UnsetValue),
